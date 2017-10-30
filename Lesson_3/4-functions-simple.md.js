bdb526eabcc5ded2b7bc55cd9855902504680352
kseniyakukhlevska <32741671+kseniyakukhlevska@users.noreply.github.com>
2017-10-30 13:28:52 +0100

    added 4,5

--- Lesson_3/4-functions-simple.md.js
+++ Lesson_3/4-functions-simple.md.js
@@ -12,38 +12,92 @@ alert(showName(""));
 // Create function double(x) that returns the number passed to it as an argument, multiply with 2
 
 function double(x) {
-    return x*=2
+    return x *= 2
 }
-
 console.log(double(6)); // logs in console => 12
 
 // 3 task
 // Create function range(x, y) that returns a sum of digits in the range of numbers [x, y];
 
 function range(x, y) {
-// your code here
-    return 
-}
+    var lower = Math.min(x, y);
+    var upper = Math.max(x, y);
 
-range(1,3); // logs in console => 6
+    var count = 0;
+
+    for (var i = lower; i <= upper; i++) {
+        count += i;
+    }
+    return count;
+}
+console.log(range(1,3)); // logs in console => 6
 
 
 // 4 task
 // Напишите функцию которая будет находить сумму простых чисел меньших переданному в нее значению. Пример работы:
-//
-//     function getPrimesSumBelow(x, y) {
-// // your code here
-//     }
-//
-// getPrimesSumBelow(10); // 2 + 3 + 5 + 7 = 17
+var sum = 0;
+function getPrimesSumBelow(x) {
+    for (var i = 2; i < x; i++) {
+        for (var m = 2; m <= i; m++) {
+            if (i === m) {
+                sum = sum + i;
+            }
+            else if (i % m === 0) {
+                break;
+            }
+        }
+    }
+    console.log(sum);
+}
+getPrimesSumBelow(10); // 2 + 3 + 5 + 7 = 17
+
+
 // 5 task
-//
 // Create a function that returns true if number is odd, and false if even;
-//
+
+function number(i) {
+    i = +prompt("enter number");
+    if (i % 2 !== 0)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(number());
+
+
 // 6 task
-//
 // Create a function that returns all odd numbers in range, use the previous function inside this one;
-//
+
+function range(x, y) {
+    x = +prompt("enter number");
+    y = +prompt("enter number");
+
+    var arr = [];
+    for (i = x; i < y; i++) {
+        if (i % 2 !== 0)
+        {
+            arr.push(i);
+        }
+
+    }
+    console.log(arr);
+}
+
+range();
+
 // 7 task
-//
 // Create a function that will call promt window that will ask for a number to user, until he enters 9;
+
+function callnumber(number) {
+    do {
+        var number = +prompt('enter number');
+    }
+    while (number !== 9);
+}
+
+callnumber();
+
---
+++ Lesson_3/5-loops-statements.md.js
@@ -0,0 +1,93 @@
+// 1 task
+// Write a JavaScript for loop that will iterate from 0 to 15.
+// For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+for (i = 0; i < 16; i++) {
+    if (i % 2 == 0) {
+        console.log(i + ' is even');
+    }
+    else {
+        console.log(i + ' is odd');
+    }
+}
+
+//2 task
+// Write a JavaScript program which iterates the integers from 1 to 100.
+// But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz".
+
+for (i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 5 == 0) {
+        console.log('Buzz');
+    }
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else {
+        console.log(i);
+    }
+}
+
+//3 task
+// Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers
+
+var a = +prompt('enter first value');
+var b = +prompt('enter second value');
+var gcd;
+while (a!=b)
+{
+    if (a>b)
+    {
+        a = a -b;
+    }
+    else
+    {
+        b = b - a;
+    }
+}
+gcd = a;
+console.log(gcd);
+
+//4 task
+// Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+var arr = [];
+for (i = 0; i < 1000; i++) {
+    if (i % 5 == 0 || i % 3 == 0) {
+        arr.push(i);
+    }
+}
+var count = 0;
+for (e in arr) {
+    count += arr[e];
+}
+console.log(count);
+
+// 5 task
+// Console log all numbers from range 10-90 that divide to 5 and 3.
+
+for (i = 10; i < 91; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+// 6 task
+// Console log only odd numbers from range 11-70 in reverse order starting with 70.
+
+for (i = 70; i > 10; i--) {
+    if (i % 2 !== 0) {
+        console.log(i + ' is odd');
+    }
+}
+
+// 7 task
+// Console log only even numbers that divide to 2 and 3 from range 13-78 in reverse order.
+
+for (i = 78; i > 12; i--) {
+    if (i % 2 == 0 && i % 3 == 0) {
+        console.log(i + ' is even');
+    }
+}
