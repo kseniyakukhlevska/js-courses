137b7122c461187a901e3fb4878863050438af6b
kseniyakukhlevska <32741671+kseniyakukhlevska@users.noreply.github.com>
2017-11-21 15:05:32 +0200

    UPD

--- Lesson_4/arrays.md.js
+++ Lesson_4/arrays.md.js
@@ -28,7 +28,7 @@ console.log(isPal('123212')); //false
 
 var arr = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
 function anClean(arr) {
- var obj = {};
+    var obj = {};
     for (var i = 0; i < arr.length; i++) {
         var sorted = arr[i].toLowerCase().split("").sort().join("");//приводим к нижнему регистру,делим побуквенно,сортируем по алфавиту буквы в елементе массива и соединяем
         obj[sorted] = arr[i];//сравниваем
@@ -64,7 +64,7 @@ function count(){
     for (var i = 1; i <= 100 ; i++) {
         sum += i;
     }
-     console.log(sum);
+    console.log(sum);
 }
 
 count()//5050
@@ -79,7 +79,7 @@ function count(){
             sum += i;
         }
     }
-     console.log(sum);
+    console.log(sum);
 }
 
 count()//2550
@@ -94,8 +94,8 @@ count()//2550
 
 function callMe(x,y,z) {
     if(!isNaN(x) && !isNaN(y) && !isNaN(z)) {
-var sum = x + y + z;
-alert(sum);
+        var sum = x + y + z;
+        alert(sum);
     } else {
         console.error("Error! Error!");
         return null;
@@ -117,14 +117,14 @@ callMeAgain(arr);//dddddd,fff,rrrr
 // 7 задание
 // Дан массив объектов:
 
-    var arr = [
-         {name: "L1", age: 45},
-         {name: "L1", age: 20},
-         {name: "L1", age: 10},
-         {name: "L1", age: 78},
-         {name: "L1", age: 41},
-         {name: "L1", age: 10}
-    ];
+var arr = [
+    {name: "L1", age: 45},
+    {name: "L1", age: 20},
+    {name: "L1", age: 10},
+    {name: "L1", age: 78},
+    {name: "L1", age: 41},
+    {name: "L1", age: 10}
+];
 // Отсортируйте массив по полю age
 function compareAge(personA, personB) {
     return personA.age - personB.age;
@@ -186,7 +186,19 @@ result(3);//8
 // 10 task
 // Дан массив ['1', '2', '3', '4', '5', '6']. Сделайте из него строку '16-25-34'. Массив, конечно же, может быть любым.
 var arr = ['1', '2', '3', '4', '5', '6'];
+var newArr = [];
+function newStr() {
+    for (i = 0; i < arr.length; i++) {
+        var x = arr.shift();//Удаляет из массива первый элемент и возвращает первый элемент
+        var y = arr.pop();//Удаляет последний элемент из массива и возвращает последний элемент
+        i--;
+        newArr.push(x+y);//Добавляет элемент в конец массива
+    }
+    var str = newArr.join('-');//соединяем в строку через '-'
+    console.log(str);
+}
 
+newStr();//16-25-34
 
 
 
@@ -197,30 +209,115 @@ var arr = ['1', '2', '3', '4', '5', '6'];
 // Создайте новый массив, заполненный суммами соответствующих элементов, то есть [1+4, 2+5, 3+6] = [5, 7, 9].
 // Скрипт должен работать для любых массивов, даже если их длина не одинакова (подумайте, что делать в этом случае).
 
+ var arr1 = [1,2,3];
+ var arr2 = [4,5,6];
+ var newArr = [];
+
+function SumArr() {
+    var newArrLength = arr1.length;
+    if (arr2.length > arr1.length) {
+        newArrLength = arr2.length;
+    }
+    for (i = 0; i < newArrLength; i++) {
+        var sum = (arr1[i] || 0) + (arr2[i] || 0);
+        newArr.push(sum);
+    }
+    console.log(newArr);
+}
+
+SumArr();
 
 
 // 12 task
 // Дан массив. Напишите функцию, которая будет удалять элемент из массива по его тексту.
 //     Пример: arr = [1,2,3,4,3,3]. Удалим все элементы с содержимым 3: func(arr, 3).
 
-
+var arr = [1,2,3,4,3,3];
+var newArr = [];
+function deleteAllSimilar (x) {
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] === x) {
+     arr.splice(i, 1);
+     i--;
+    }
+}
+    console.log(arr);
+}
+ deleteAllSimilar(3);
 
 // 13 task
 // Реализуйте функцию intersection, вернёт массив из элементов, встречающихся в каждом из переданных массивов.
 //     Пример: intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]) вернёт [1, 2].
 
+arr = [];
+function intersection(arr1, arr2, arr3) {
+    for (i = 0; i < arr1.length; i++) {
+        for (j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                for (n = 0; n < arr3.length; n++) {
+                    if (arr1[i] === arr3[n]) {
+                        arr.push(arr1[i]);
+                    }
+                }
+            }
+        }
+    }
+    console.log(arr);
+}
+
+intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]);// [1, 2]
 
 // 14 task
-// Дан массив с числами, к примеру [2, 3, 1, 4]. Сделайте из него массив [2, 2, 3, 3, 3, 1, 4, 4, 4, 4]. В массиве могут быть только целые положительные числа.
+// Дан массив с числами, к примеру [2, 3, 1, 4]. Сделайте из него массив [2, 2, 3, 3, 3, 1, 4, 4, 4, 4].
+// В массиве могут быть только целые положительные числа.
 
+var arr = [2, 3, 1, 4];
+var newArr = [];
+function createMore() {
+    for (i = 0; i < arr.length; i++) {
+        for (k = 0; k < arr[i]; k++) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr);
+}
 
+createMore();// [2, 2, 3, 3, 3, 1, 4, 4, 4, 4]
 
 // 15 task
-// Реализуйте функцию, которая будет дополнять массив заданными элементами до заданного размера. Пример: [1,2,3] дополним нулями до размера 6 - [1,2,3,0,0,0].
+// Реализуйте функцию, которая будет дополнять массив заданными элементами до заданного размера.
+// Пример: [1,2,3] дополним нулями до размера 6 - [1,2,3,0,0,0].
 
+var arr = [1,2,3];
+function newArr(x, y) {
+    for (i = arr.length; i < y; i++) {
+        arr.push(x);
+    }
+    console.log(arr);
+}
+
+newArr(0, 6);//[1, 2, 3, 0, 0, 0]
 
 
 // 16 task
 // Дан массив с числами. Не используя цикл, выведите:
+
 // Три самых маленьких числа в массиве (числа должны быть разные, дубли должны быть проигнорированы).
+
+var arr = [1, 4, 6, -3, 5, 14, 4, 3, 8, 77, 3];
+
+arr = arr.sort(function (x,y) {
+    return x - y;
+});
+var newArr = arr.slice(0,3);
+console.log(newArr);//[-3, 1, 3]
+
 // Первые 3 отрицательных числа в массиве (по порядку следования).
+
+var arr = [1, -4, 6, -3, 5, 14, 4, 3, -8, 77, 3];
+
+function negativeNumber(i) {
+    return i < 0;
+}
+var numbers = arr.filter(negativeNumber).slice(0,3);
+console.log(numbers);// [-4, -3, -8]
