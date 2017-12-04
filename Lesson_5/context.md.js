e77d97f287288c6d9eed146cf644793a5d168913
kseniyakukhlevska <32741671+kseniyakukhlevska@users.noreply.github.com>
2017-12-04 17:07:10 +0200

    UPD

--- Lesson_5/context.md.js
+++ Lesson_5/context.md.js
@@ -2,30 +2,30 @@
 //
 // Опеределить на какой контекст будет ссылатся this в результате выполнений следующих примеров
 //
-// function isContextEqualTo(contextLink) {
-//     console.log(contextLink === this);
-// }
-//
-// function name() {
-//     isContextEqualTo(window); // ??
-// }
-//
-// var name = "Vasya";
-//
-// var user = {
-//     getName: function () {
-//         this.getContext(window); // ??
-//         this.getContext(user); // ??
-//     },
-//     getContext: isContextEqualTo
-// }
-//
-// console.log(user.getName()); // ??
-//
-// var getName = user.getName;
-//
-// console.log(getName()); // ??
-//
-// user.getName = name;
-//
-// console.log(user.getName()); // ??
+function isContextEqualTo(contextLink) {
+    console.log(contextLink === this);
+}
+
+function name() {
+    isContextEqualTo(window); // true - context window
+}
+
+var name = "Vasya";
+
+var user = {
+    getName: function () {
+        this.getContext(window); // true - context window
+        this.getContext(user); // false - context window
+    },
+    getContext: isContextEqualTo
+}
+
+console.log(user.getName()); // true - context window, false - context window
+
+var getName = user.getName;
+
+console.log(getName()); // window
+
+user.getName = name;
+
+console.log(user.getName()); // window
